fd60334cab4043195320221c5c9eda0c702a43ff
mie-h <56993977+mie-h@users.noreply.github.com>
2022-08-17 19:43:26 -0700

    react counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from"react";
+import Tweet from "./Tweet";
 
-function App() {
+function App(){
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100)
+  }, []);
+
+  
+  const [users, setUsers] = useState([
+    {name: "name1", message: "happy"},
+    {name: "name2", message: "healthy"},
+    {name: "name3", message: "lucky"}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello React</h1>
+      
+      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount+1)}>+</button>
+
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
---
+++ src/Tweet.js
@@ -0,0 +1,11 @@
+
+function Tweet({name, message}){
+    return (
+        <div className="tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default Tweet;
